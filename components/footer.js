546c12053d1ec27916cc7490965b0c8dca6a47fe
Lachlan Campbell <lachlanjc@hey.com>
2020-12-01 16:47:02 -0500

    Edit footer

--- components/footer.js
+++ components/footer.js
@@ -1,18 +1,23 @@
 import { Container, Box } from 'theme-ui'
+import { GitHub } from 'react-feather'
 
 const Footer = () => (
   <Box as="footer" sx={{ textAlign: 'center', px: 2, py: [3, 4] }}>
     <Container
       sx={{
         maxWidth: 640,
+        display: 'flex',
         alignItems: 'center',
         justifyContent: 'center',
         color: 'muted',
         fontSize: 1,
-        a: { color: 'inherit' }
+        svg: { fill: 'currentColor', mr: 3 }
       }}
+      as="a"
+      href="https://github.com/lachlanjc/catherine"
     >
-      <a href="https://github.com/lachlanjc/catherine">Open source</a>
+      <GitHub />
+      View source code
     </Container>
   </Box>
 )
