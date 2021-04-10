555801576b84a4c747940d58c9deb8469d3eccc6
Lachlan Campbell <lachlanjc@hey.com>
2021-04-10 14:44:23 -0400

    Upgrade dependencies

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 const withMDX = require('@next/mdx')({ extension: /\.mdx?$/ })
 module.exports = withMDX({
-  pageExtensions: ['js', 'mdx']
+  future: { webpack5: true },
+  pageExtensions: ['js', 'mdx'],
 })
