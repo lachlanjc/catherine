b245ce0255b26fd159f6b62d8f0c4f974551230c
Lachlan Campbell <lachlanjc@hey.com>
2021-07-03 20:31:47 -0400

    Fix webpack5 flag

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 const withMDX = require('@next/mdx')({ extension: /\.mdx?$/ })
 module.exports = withMDX({
-  future: { webpack5: true },
   pageExtensions: ['js', 'mdx'],
   async rewrites() {
     return [{ source: '/cv', destination: '/cv/catherine_campbell.pdf' }]
