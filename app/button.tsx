ef338272601e047db7272277798c3e4e2f9bb358
Lachlan Campbell <lachlanjc@hey.com>
2025-01-27 23:29:39 -0500

    Upgrade to Tailwind v4

--- app/button.tsx
+++ app/button.tsx
@@ -8,7 +8,7 @@ export default function Button({
   return (
     <a
       {...props}
-      className={`flex items-center gap-3 no-underline border border-stone-400 dark:border-stone-600 rounded-lg px-4 py-2 shadow transition-shadow hover:shadow-md`}
+      className={`flex items-center gap-3 no-underline border border-stone-400 dark:border-stone-600 rounded-full px-4 py-1 shadow-sm transition-shadow hover:shadow-md`}
     />
   )
 }
