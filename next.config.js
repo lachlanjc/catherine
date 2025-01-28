f0cb2a40f5844a4f988f2745be1cf8a0ffa9a318
Lachlan Campbell <lachlanjc@hey.com>
2025-01-27 23:43:38 -0500

    Disable mdxRs

--- next.config.js
+++ next.config.js
@@ -3,9 +3,6 @@ const withMDX = require('@next/mdx')()
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['ts', 'tsx', 'mdx'],
-  experimental: {
-    mdxRs: true,
-  },
   async redirects() {
     return [
       {
