e2b882d4f8669cfe6f427e488a10d86079c04e6b
Lachlan Campbell <lachlanjc@hey.com>
2025-01-27 23:49:39 -0500

    Revert "Disable mdxRs"
    This reverts commit f0cb2a40f5844a4f988f2745be1cf8a0ffa9a318.

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const withMDX = require('@next/mdx')()
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['ts', 'tsx', 'mdx'],
+  experimental: {
+    mdxRs: true,
+  },
   async redirects() {
     return [
       {
